40383786a7474861e52dbd21dfa9e9e1560f00f0
1NBuk <anbukareva@mail.ru>
2026-01-24 23:27:30 +0300

    changed root in run_pipeline.py

--- etl/run_pipeline.py
+++ etl/run_pipeline.py
@@ -2,9 +2,9 @@ import os
 import subprocess
 from datetime import datetime
 
-BASE_DIR = os.path.dirname(__file__)
-PARSERS_DIR = os.path.join(BASE_DIR, "..", "parsers")
-PYTHON_EXEC = r"C:\Users\User\PycharmProjects\parse_sales\venv\Scripts\python.exe"
+REPO_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PARSERS_DIR = os.path.join(REPO_ROOT, "parsers")
+PYTHON_EXEC = "python3"  # в GitHub Actions можно просто python3
 
 def run_all_parsers():
     parser_files = [f for f in os.listdir(PARSERS_DIR) if f.endswith("_parser.py")]
@@ -19,14 +19,8 @@ def run_all_parsers():
         if result.returncode != 0:
             print(f"Ошибка при запуске {parser}: {result.stderr}")
 
+
 if __name__ == "__main__":
     print(f"Запуск ETL pipeline: {datetime.now()}")
     run_all_parsers()
-    print(f"Парсеры завершены: {datetime.now()}")
-
-    print("Загрузка данных в Google Drive")
-    subprocess.run(
-        [PYTHON_EXEC, os.path.join(BASE_DIR, "upload_to_gdrive.py")],
-        check=True
-    )
     print(f"ETL pipeline завершён: {datetime.now()}")
