ddabad742fb7e9691860c2a9f8b0f4a694964705
1NBuk <anbukareva@mail.ru>
2026-01-24 23:30:50 +0300

    changed root in run_pipeline.py 2

--- etl/run_pipeline.py
+++ etl/run_pipeline.py
@@ -3,10 +3,13 @@ import subprocess
 from datetime import datetime
 
 REPO_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-PARSERS_DIR = os.path.join(REPO_ROOT, "parsers")
-PYTHON_EXEC = "python3"  # в GitHub Actions можно просто python3
+PARSERS_DIR = os.path.join(REPO_ROOT, "etl", "parsers")  # <-- внутри etl
+PYTHON_EXEC = "python3"
 
 def run_all_parsers():
+    if not os.path.exists(PARSERS_DIR):
+        raise FileNotFoundError(f"Папка с парсерами не найдена: {PARSERS_DIR}")
+
     parser_files = [f for f in os.listdir(PARSERS_DIR) if f.endswith("_parser.py")]
 
     for parser in parser_files:
@@ -18,7 +21,6 @@ def run_all_parsers():
         print(result.stdout)
         if result.returncode != 0:
             print(f"Ошибка при запуске {parser}: {result.stderr}")
-
 
 if __name__ == "__main__":
     print(f"Запуск ETL pipeline: {datetime.now()}")
